c4e7306b5a86037b9ab54c15ee0d744b7a982547
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-10-22 12:15:03 -0700

    if when multiple gems (without ng-repeat directive)

--- app.js
+++ app.js
@@ -7,17 +7,26 @@
   // module
   // dependency
 
-  var gem = {
-    name : 'Dodecahedron',
-    price : 2.95,
-    description : 'Some gems have hidden qualities \
-      beyond their luster, beyond their shine... Dodeca is one of those gems.',
-    canPurchase: true,
-    soldOut: true,
-  }
-
+  var gems = [
+    {
+      name : 'Dodecahedron',
+      price : 2.95,
+      description : 'Some gems have hidden qualities \
+        beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+      soldOut: true,
+    },
+    {
+      name : 'Pentagonal',
+      price : 5.95,
+      description : 'Some gems have hidden qualities \
+        beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: false,
+      soldOut: true,
+    }
+  ]
   app.controller('StoreController', function(){
-    this.product = gem; // 'product' property of StoreController
+    this.products = gems; // 'product' property of StoreController
   });
 }
 
